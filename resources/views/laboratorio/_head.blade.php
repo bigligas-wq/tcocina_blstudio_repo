1fe93a6f378e3893ee3ddfc7731a0f0a8f8a5ffa
Juan Ignacio Ibarlucia <grandesligasarg@gmail.com>
2026-05-31 21:57:15 -0300

    fix: cargar laboratorio-redesign.css en _head del laboratorio

--- resources/views/laboratorio/_head.blade.php
+++ resources/views/laboratorio/_head.blade.php
@@ -3,5 +3,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Mono:wght@400;500&family=DM+Sans:wght@400;500;600&family=Instrument+Sans:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/laboratorio.css') }}?v={{ filemtime(public_path('css/laboratorio.css')) }}">
+    <link rel="stylesheet" href="{{ asset('css/laboratorio-redesign.css') }}?v={{ filemtime(public_path('css/laboratorio-redesign.css')) }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js" defer></script>
 @endpush
